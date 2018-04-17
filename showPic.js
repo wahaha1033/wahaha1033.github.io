c99a3eb1cf0062ea26bd84689719da2107163725
wahaha1033 <37521526+wahaha1033@users.noreply.github.com>
2018-04-17 22:43:28 +0800

    Add files via upload

---
+++ showPic.js
@@ -0,0 +1,5 @@
+function showPic(whichpic){
+  var source = whichpic.getAttribute("href");
+   var placeholder = document.getElementById("placeholder");
+    placheholder.setAttribute("src",source);
+}
